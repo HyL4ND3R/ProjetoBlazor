97ad48a44cd2a0c28d954456b88800f3ee10a40c
Jean Scholz Konig <jeanbock04.2@hotmail.com>
2026-03-06 18:27:48 -0300

    Criadas classes modelo do pedido e pedidoitem

---
+++ ProjetoBlazor/Models/Pedido.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoBlazor.Models
+{
+    public class Pedido
+    {
+        [Key]
+        public int Controle { get; set; }
+        public int Codigo { get; set; }
+        public int ClienteCodigo { get; set; }
+        public DateTime Data { get; set; }
+        public decimal QtdeTotal { get; set; }
+        public decimal ValorTotal { get; set; }
+
+    }
+}
---
+++ ProjetoBlazor/Models/PedidoItem.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoBlazor.Models
+{
+    public class PedidoItem
+    {
+        [Key]
+        public int Controle { get; set; }
+        public int ControlePedido { get; set; }
+        public int Item { get; set; }
+        public int ProdutoCodigo { get; set; }
+        public string Descricao { get; set; } = "";
+        public decimal Quantidade { get; set; }
+        public decimal ValorUn { get; set; }
+        public decimal ValorTotal { get; set;}
+    }
+}
