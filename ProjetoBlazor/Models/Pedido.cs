0dc4f0a68821d206a5ade36ab06a68cd73661508
Jean Scholz König <141757464+HyL4ND3R@users.noreply.github.com>
2026-03-09 20:15:12 -0300

    Ajustes For Pedido

--- ProjetoBlazor/Models/Pedido.cs
+++ ProjetoBlazor/Models/Pedido.cs
@@ -1,16 +1,16 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Numerics;
 
 namespace ProjetoBlazor.Models
 {
     public class Pedido
     {
         [Key]
-        public int Controle { get; set; }
+        public Int64 Controle { get; set; }
         public int Codigo { get; set; }
         public int ClienteCodigo { get; set; }
         public DateTime Data { get; set; }
         public decimal QtdeTotal { get; set; }
         public decimal ValorTotal { get; set; }
-
     }
 }
--- ProjetoBlazor/Models/PedidoItem.cs
+++ ProjetoBlazor/Models/PedidoItem.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Numerics;
 
 namespace ProjetoBlazor.Models
 {
@@ -6,7 +7,7 @@ namespace ProjetoBlazor.Models
     {
         [Key]
         public int Controle { get; set; }
-        public int ControlePedido { get; set; }
+        public Int64 ControlePedido { get; set; }
         public int Item { get; set; }
         public int ProdutoCodigo { get; set; }
         public string Descricao { get; set; } = "";
