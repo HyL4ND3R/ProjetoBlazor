7517454c5f26ff5857adaad8859462dbae27f685
Jean Scholz König <141757464+HyL4ND3R@users.noreply.github.com>
2026-03-11 21:20:12 -0300

    Ajustado index do pedido com o nome do cliente e formatação de Data e Moeda, incluido atribudo Not Mapped na classe pedido para armazenar o nome do cliente

--- ProjetoBlazor/Models/Pedido.cs
+++ ProjetoBlazor/Models/Pedido.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Numerics;
 
 namespace ProjetoBlazor.Models
@@ -9,6 +10,8 @@ namespace ProjetoBlazor.Models
         public Int64 Controle { get; set; }
         public int Codigo { get; set; }
         public int ClienteCodigo { get; set; }
+        [NotMapped]
+        public string? ClienteNome { get; set; } = "";
         public DateTime Data { get; set; }
         public decimal? QtdeTotal { get; set; } = 0;
         public decimal? ValorTotal { get; set; } = 0;
