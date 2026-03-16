fb203decb71fa33eab6c49a9a198b6c8e7d6aadd
Jean Scholz Konig <jeanbock04.2@hotmail.com>
2026-03-16 13:06:10 -0300

    ajustes frm Rel Pedidos, feitos campos de data e busca de cliente e produto

---
+++ ProjetoBlazor/Models/RelatorioPedidos.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjetoBlazor.Models
+{
+    public class RelatorioPedidos
+    {
+        public DateTime DataInicial { get; set; }
+        public DateTime DataFinal { get; set; }
+        public int? CodCliente { get; set; }
+        public int? CodProduto { get; set; }
+    }
+}
